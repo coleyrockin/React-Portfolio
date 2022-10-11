566e2997a846d6a5d82c0b1af68e53f1203a656e
coleyrockin <coleyrockin@aol.com>
2022-10-11 17:03:46 -0400

    About me structure

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section class="about">
+      <h1 id="about">About Boyd Roberts</h1>
+      <p>I am full-stack developer...</p>
+    </section>
+  );
+}
+
+export default About;
