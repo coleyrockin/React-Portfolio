59be52a0986eda53b92d5b907552f02dad20596e
coleyrockin <coleyrockin@aol.com>
2022-11-07 18:44:10 -0600

    Little improvments

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -13,13 +13,13 @@ function Resume() {
         <li className="flex items-center">Javascript ES6 <FaJsSquare size={16} /></li>
         <li className="flex items-center">MongoDB <Mongodb size={16} /></li>
         <li className="flex items-center">MySQL <Mysql size={20} /></li>
-        <li className="flex items-center">React <ReactJs size={16} color="#61DAFB" /></li>
+        <li className="flex items-center">React <ReactJs size={16} /></li>
         <li className="flex items-center">Bootstrap <FaBootstrap size={16} /> | BULMA <Bulma size={16} /> | Tailwind <Tailwindcss size={16} /></li>
         <li className="flex items-center">Node.Js < Nodedotjs size={16} /> & various packages</li>
         <li className="flex items-center">Express < Express size={16} /></li>
         <li className="flex items-center">Git < Git size={16} /></li>
         <li className="flex items-center">Database Theory <FaDatabase size={14} /></li>
-        <li className="flex items-center">Responsive design <DiResponsive size={20} /></li>
+        <li className="flex items-center">Responsive Design <DiResponsive size={20} /></li>
 
 
       </ul>
