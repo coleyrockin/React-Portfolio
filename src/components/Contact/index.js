432fb41032211cc9c9579cea12e0e7988f7c3139
coleyrockin <coleyrockin@aol.com>
2022-11-13 19:26:16 -0600

    Contact form wideness edited

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -53,11 +53,11 @@ function Contact() {
   };
   //HTML FOR CONTACT
   return (
-    <div className="Contact w-96">
-      <div class="form w-85">
+    <div className="Contact">
+      <div class="form">
         <h1 className="text-center text-xl m-2">Contact Me</h1>
-        <form enctype="multipart/form-data" onSubmit={handleSubmit}>
-          <div class="form-group w-full">
+        <form class="w-12/12" enctype="multipart/form-data" onSubmit={handleSubmit}>
+          <div class="form-group">
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               type="text"
