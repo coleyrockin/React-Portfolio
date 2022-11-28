3cb5a89ea30cdf41d54fcfeddf92e85b747d27ce
coleyrockin <coleyrockin@aol.com>
2022-11-28 13:30:01 -0600

    CNAME

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
     <section class="about" className="flex flex-row">
 
       <img className="center object-contain" height={160} width={160} src={ImgMe} alt="Boyd" />
-      <p className="p-2">Full Stack Web Developer that earned a certificate from Southern Methodist University Coding Boot Camp. Specializing in the MERN stack, and aspiring to learn more computer languages, expand my knowledge, and become a Software Engineer.</p>
+      <p className="p-2">Full Stack Web Developer with certificate from Southern Methodist University Coding Boot Camp. Specializing in the MERN stack, and aspiring to learn more computer languages, expand my knowledge, and become a Software Engineer.</p>
     </section >
   );
 }
